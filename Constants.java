686e4816b8f6fc37387564e6d2674d357ee15fcf
RakibHossen490 <mdrakibhossenhowladar@gmail.com>
2024-11-28 09:33:33 +0600

    Constant.java

---
+++ Constants.java
@@ -0,0 +1,8 @@
+public class Constants {
+    public static final String FILE_PATH = "employees.txt";
+    public static final String DATA_LOADING = "Loading data ...";
+    public static final String DATA_LOADED = "Data Loaded.";
+    public static final String INVALID_ARGUMENT = "Invalid argument. Please provide a valid command.";
+    public static final String EMPLOYEE_NOT_FOUND = "Employee not found.";
+    public static final String EMPLOYEE_FOUND = "Employee found!";
+}
