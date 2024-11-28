a649445ac39633deeb8198d18a48feca307fa54a
RakibHossen490 <mdrakibhossenhowladar@gmail.com>
2024-11-28 09:42:58 +0600

    Constant Change

--- Constants.java
+++ Constants.java
@@ -3,6 +3,6 @@ public class Constants {
     public static final String DATA_LOADING = "Loading data ...";
     public static final String DATA_LOADED = "Data Loaded.";
     public static final String INVALID_ARGUMENT = "Invalid argument. Please provide a valid command.";
-    public static final String EMPLOYEE_NOT_FOUND = "Employee not found.";
-    public static final String EMPLOYEE_FOUND = "Employee found!";
+    public static final String EMPLOYEE_NOT_FOUND="employee not found.";
+    public static final String EMPLOYEE_FOUND="Employee Found.";
 }
